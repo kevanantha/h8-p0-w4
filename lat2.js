f4c7e100f840829726b145d1b65e41f6df38bb78
Kevin Anantha <kevin.anantha@gmail.com>
2019-08-09 20:54:08 +0700

    :rocket:

--- lat2.js
+++ lat2.js
@@ -50,5 +50,5 @@ console.log(twoArrayOperation([-4, 5, 57, 33, -90], [4, 2, 54, 24, 34])) // [ 0,
 console.log(twoArrayOperation([12, 34, 11, 23, 12], [11, 78, 45, 20, 42])) // [ 23, -44, 56, 3, 54 ]
 console.log(twoArrayOperation([1, 2, 3], [1, 2, 3])) // [ 2, 0, 6 ]
 console.log(twoArrayOperation([-4, 5, 57, 33, -90], [4, 2, 54, 24, 34, 17])) // [0,  3, 111, 9, -56, -17]
-console.log(twoArrayOperation([-4, 5, 57, 33, -90], [4, 2, 54, 24, 34, 17, 21, 21])) // [0,  3, 111, 9, -56, -17]
-console.log(twoArrayOperation([-1, 2, -3, 4, -5], [-4, -3, 2, 1])) // [0,  3, 111, 9, -56, -17]
+console.log(twoArrayOperation([-4, 5, 57, 33, -90], [4, 2, 54, 24, 34, 17, 21, 21])) // [0,  3, 111, 9, -56, -17, 21, -21]
+console.log(twoArrayOperation([-1, 2, -3, 4, -5], [-4, -3, 2, 1])) // [-5, 5, -1, 3, -5]
