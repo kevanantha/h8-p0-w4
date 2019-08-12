ebb49a46b432e83fc16cef24360f7e04a8c205a8
Kevin Anantha <kevin.anantha@gmail.com>
2019-08-12 22:20:01 +0700

    :rocket:

--- digitPerkalianMin.js
+++ digitPerkalianMin.js
@@ -1,7 +1,5 @@
 function digitPerkalianMinimum(angka) {
   var length = Math.round(Math.sqrt(angka))
-  console.log(Math.sqrt(angka))
-  length
   var min = Number.MAX_VALUE
 
   for (var i = 1; i <= length; i++) {
@@ -15,13 +13,12 @@ function digitPerkalianMinimum(angka) {
       if (n3 < min) min = n3
     }
   }
-  min
   return min
 }
 
 // TEST CASES
 console.log(digitPerkalianMinimum(24)) // 2
-// console.log(digitPerkalianMinimum(90)) // 3
-// console.log(digitPerkalianMinimum(20)) // 2
-// console.log(digitPerkalianMinimum(179)) // 4
-// console.log(digitPerkalianMinimum(1)) // 2
+console.log(digitPerkalianMinimum(90)) // 3
+console.log(digitPerkalianMinimum(20)) // 2
+console.log(digitPerkalianMinimum(179)) // 4
+console.log(digitPerkalianMinimum(1)) // 2
